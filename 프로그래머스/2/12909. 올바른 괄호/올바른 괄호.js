4ac96ef9206189f2cc71c81a4ff1478ed01194b0
Séyoon Jeong <seyoonagain@gmail.com>
2024-10-11 11:46:49 +0900

    [level 2] Title: 올바른 괄호, Time: 31.87 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,12 +1,9 @@
 function solution(s){
     const parens = [...s]
-    if(parens.at(-1) === '(') return false
-    const countParens = (targetParen) => parens.filter(paren => paren === targetParen).length
-    if(countParens('(') !== countParens(')')) return false
     const pairCheck = []
     parens.forEach(paren => {
         if(pairCheck.at(-1) === '(' && paren === ')') pairCheck.pop()
         else pairCheck.push(paren)
     })
-    return !pairCheck.length ? true : false
+    return !pairCheck.length
 }
