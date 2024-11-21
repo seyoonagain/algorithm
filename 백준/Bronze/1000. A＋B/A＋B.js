d0bb416826e5cf9ff184cb0e56df51b18197fe49
Séyoon Jeong <seyoonagain@gmail.com>
2024-11-21 23:29:39 +0900

    [Bronze V] Title: A+B, Time: 112 ms, Memory: 9448 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function(line) {
+    const [a, b] = line.split(' ').map(Number)
+    console.log(a + b)
+    rl.close();
+}).on("close", function() {
+    process.exit();
+});
